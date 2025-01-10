bf3d4eb648ffeb1b2dde69af75cf121469914369
Rafael Langsch <rla@m3par.com>
2025-01-10 16:19:16 -0300

    add AWS Secrets Manager integration to retrieve JWT access key

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,9 @@ import os, sys, re, json, logging
 
 import jwt
 
+import boto3
+
+from botocore.exceptions import ClientError
 from datetime import date, datetime, time
 from decimal import Decimal
 
@@ -322,3 +325,27 @@ async def set_schema(tenant_id, session):
         await session.execute(text("SET search_path TO public"))
     else:
         await session.execute(text(f"SET search_path TO tenant_{tenant_id}"))
+
+
+def get_secret():
+
+    secret_name = "adm/jwt-access-key"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
