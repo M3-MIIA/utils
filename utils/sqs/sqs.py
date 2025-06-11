865da7e7884ca1d7480f9ec93a6be1cce148792f
Thiago M. de C. Marques <tma@m3par.com>
2025-06-11 20:51:35 -0300

    Second draft version of SQS queue consumer framework

--- utils/sqs/sqs.py
+++ utils/sqs/sqs.py
@@ -1,66 +1,116 @@
-# Reference:
-# - https://docs.aws.amazon.com/pt_br/lambda/latest/dg/with-sqs.html
-
-
 import asyncio
 import logging
 from typing import Iterable, Tuple
 
-from ..async_utils import make_with_semaphore
+import boto3
+
+from ..async_utils import AsyncTaskLogFilter, make_with_semaphore
 
 from .sqs_consumer import _Message, SqsConsumer
 from .sqs_exceptions import SqsAbortExit, SqsBackoffExit, SqsExit
 
 
-async def _handle_sqs_exit(consumer: SqsConsumer[_Message], message: dict,
-                           cause: SqsExit):
+logging.getLogger().addFilter(AsyncTaskLogFilter())
+
+
+async def _handle_sqs_exit(consumer: SqsConsumer[_Message], raw_message: dict,
+                           message: _Message, cause: SqsExit | Exception):
     try:
-        await consumer.handle_sqs_exit(message, cause)
+        await consumer.handle_sqs_exit(raw_message, message, cause)
     except Exception | SqsExit as e:
         e.add_note("Exception raised while consumer was handling a previous SQS exit exception")
+        raise
+
+
+_sqs_client = None
+
+def _delete_sqs_message(consumer: SqsConsumer, message: dict, message_id: str):
+    global _sqs_client
+
+    try:
+        if _sqs_client is None:
+            _sqs_client = boto3.client("sqs")
+
+        _sqs_client.delete_message(
+            QueueUrl=consumer.queue_url,
+            ReceiptHandler=message["receiptHandler"]
+        )
+    except Exception as e:
+        _sqs_client = None  # Force reconnection in next attempt
+        e.add_note(f"Failed deleting message {message_id} from SQS queue")
         raise
 
 
 async def _process_sqs_message(consumer: SqsConsumer[_Message],
                                record: dict):
     message_id = record["messageId"]
+
+    message = None
     delete_message = True
+    exception = None
+
+    AsyncTaskLogFilter.task_id.set(message_id)
 
     error_message = "Failed parsing SQS message"
     try:
         message = consumer.parse_message(record)
         if custom_message_id := consumer.extract_custom_message_id(message):
             message_id = f"{message_id} ({custom_message_id})"
+            AsyncTaskLogFilter.task_id.set(message_id)
 
         error_message = "Failed processing SQS message"
         await consumer.process_job(message)
     except SqsBackoffExit as e:
+        exception = e
         delete_message = False
         logging.warning(e.compose_message(message_id))
-        await consumer.handle_sqs_exit(record, e)
     except SqsAbortExit as e:
+        exception = e
         logging.error(e.compose_message(message_id))
-        await consumer.handle_sqs_exit(record, e)
     except Exception as e:
+        exception = e
         e.add_note(f"{error_message} {message_id}")
-        await consumer.handle_sqs_exit(record, e)
-        raise
+        raise  # The exception will be logged by log_unhandled_exceptions()
     finally:
+        if exception:
+            await _handle_sqs_exit(consumer, record, message, exception)
+
         if delete_message:
-            pass  # TODO: boto3 delete message
+            _delete_sqs_message(consumer, record, message_id)
 
 
-def log_unhandled_exceptions(
+def _log_unhandled_exceptions(
         results: Iterable[Tuple[dict, BaseException | None]]
 ):
-    results = filter(lambda r: r[1] is not None, results)
-    for message, result in results:
+    exceptions = [ r for r in results if isinstance(r[1], Exception) ]
+    if not exceptions:
+        return
+
+    for message, exception in exceptions:
         message_id = message["messageId"]
         msg = f"Unhandled exception raised while processing job {message_id}"
-        logging.error(msg, exc_info=result)
+        logging.error(msg, exc_info=exception)
+
+    exceptions = [ r[1] for r in results if isinstance(r[1], Exception) ]
+    raise ExceptionGroup("Unhandled errors in SQS processing",
+                         exceptions)
 
 
 async def process_sqs_messages(consumer: SqsConsumer[_Message], event: dict):
+    """
+    Process received SQS queue messages from lambda integration `event`.
+
+    See the SqsConsumer class for more info on how the messages are processed.
+
+    If any unhandled exception (aside from `SqsExit` ones) is raised, only the
+    message in which the exception occurred is aborted: Other received messages
+    continue being processed concurrently. After all messages have been
+    processed, if any unhandled exception occurred, an ExceptionGroup exception
+    holding all unhandled exceptions is raised at the end of the processing.
+
+    More info: https://docs.aws.amazon.com/pt_br/lambda/latest/dg/with-sqs.html
+    """
+
     records = event.get("Records")
     assert records != None, "Malformed event dict (missing \"Records\" entry)"
 
@@ -74,4 +124,4 @@ async def process_sqs_messages(consumer: SqsConsumer[_Message], event: dict):
     )
 
     results = await asyncio.gather(*tasks, return_exceptions=True)
-    log_unhandled_exceptions(zip(records, results))
+    _log_unhandled_exceptions(zip(records, results))
--- utils/sqs/sqs_consumer.py
+++ utils/sqs/sqs_consumer.py
@@ -1,31 +1,151 @@
 from abc import ABC, abstractmethod
+from os import environ
 from typing import Generic, TypeVar
 
 from pydantic import BaseModel
 
-from .sqs_exceptions import SqsExit
+from .sqs_exceptions import SqsAbortExit, SqsBackoffExit, SqsExit
 
 
 _Message = TypeVar("_Message", bound=BaseModel)
 
 
 class SqsConsumer(ABC, Generic[_Message]):
+    """
+    Specify a consumer for messages from an SQS queue.
+
+    A consumer must implement the `parse_message()` method. It may also override
+    the default implementation of other methods (especially
+    `handle_sqs_error()`) as needed.
+
+    # Recommended structure
+    The recommended way to specify a consumer is:
+    1. Create a function that encapsulates only the business logic, without
+       platform specifics (e.g.: database access). Receive all input as
+       function parameters and return all outputs as function returned values.
+    2. Create a class deriving from `SqsConsumer` and override the methods
+       needed to parse the queue message, fetch any input data needed from data
+       sources like databases and convert them to the business logic function
+       input format (and the other way round to store the function output into
+       the matching data sinks).
+    3. Create the SQS queue lambda handler that calls the `process_sqs_messages()`
+       function using the `SqsConsumer` created in the previous step.
+
+    E.g.:
+    ```
+    # ❶ Business logic function
+    # --------------------------------------------------------------------------
+    @dataclass
+    class AssessEssayInput:
+        essay_theme: str
+        reference_texts: str
+        essay_text: str
+
+    @dataclasss
+    class AssessEssayOutput:
+        score: decimal
+        remarks: str
+
+    def assess_essay(essay: AssessEssayInput) -> AssessEssayOutput:
+        […]
+
+
+    # ❷ SQS queue message consumer
+    # --------------------------------------------------------------------------
+    class AssessEssaySqsMessage(pydantic.BaseModel):
+        id: int
+
+    async def fetch_essay_from_db(id: int) -> AssessEssayInput:
+        […]
+
+    async def store_assessment_in_db(id: int,
+                                     assessment: AssessEssayOutput) -> None:
+        […]
+
+    async def store_assessment_error_in_db(id: int, error_code,
+                                           error_message) -> None:
+        […]
+
+    class AssessEssaySqsConsumer(SqsConsumer[AssessEssaySqsMessage]):
+        @override
+        async def process_job(self, message: AssessEssaySqsMessage) -> None:
+            input_data = fetch_essay_from_db(message.id)
+            output_data = assess_essay(input_data)  # ⬅️ Call the business logic function
+            store_assessment_in_db(output_data)
+
+        @override
+        async def handle_sqs_error(self, raw_message: dict,
+                                  message: AssessEssaySqsMessage,
+                                  cause: SqsExit | Exception,
+                                  error_code: str, error_message: str) -> None:
+            store_assessment_error_in_db(message.id, error_code, error_message)
+
+
+    # ❸ SQS queue lambda handler
+    # --------------------------------------------------------------------------
+    def lambda_handler(event: dict, context):
+        loop = asyncio.get_event_loop()
+        sqs_consumer = AssessEssaySqsConsumer()
+        loop.run_until_complete(process_sqs_messages(sqs_consumer, event))
+    ```
+    Note in the example above how the `assess_essay()` business logic function
+    is isolated from platform details of data sources and sinks. This allows
+    this same function to be called in various contexts, like:
+    1. In unit tests with fixed input data
+    2. In local runs with data read from and written to files in the filesystem
+    3. Queue event handlers with data stored in databases
+    """
+
+    def __init__(self, queue_url: str | None = None) -> None:
+        super().__init__()
+        if queue_url is None:
+            queue_url = environ.get("MIIA_SQS_QUEUE_URL")
+
+        assert queue_url, "Missing SQS queue URL"
+        self.queue_url = queue_url
+
+
     @property
     def max_parallel_tasks(self) -> int | None:
         return None  # Use default from `make_with_semaphore()` (see `process_sqs_messages()`)
+
 
     def parse_message(self, message: dict) -> _Message:
         raw_body = message["body"]
         body = _Message.model_validate_json(raw_body)
         return body
 
+
     def extract_custom_message_id(self, message: _Message) -> str | None:
         return getattr(message, "id", None)
+
 
     @abstractmethod
     async def process_job(self, message: _Message) -> None:
         ...
 
-    async def handle_sqs_exit(self, message: dict,
+
+    async def handle_sqs_exit(self, raw_message: dict, message: _Message | None,
                               cause: SqsExit | Exception) -> None:
+        if (
+            message is None  # Has no message content to match with the error
+            or isinstance(cause, SqsBackoffExit)  # Will try again, it's not the
+                                                  # final state of the message
+        ):
+            return  # Don't treat as error
+
+        if isinstance(cause, SqsAbortExit):
+            error_code = cause.error_code
+            error_message = cause.error_message
+        else:
+            error_code = "internal_server_error"
+            error_message = "Internal server error"
+
+        await self.handle_sqs_error(raw_message, message, cause, error_code,
+                                    error_message)
+
+
+    async def handle_sqs_error(self, raw_message: dict, message: _Message,
+                               cause: SqsExit | Exception,
+                               error_code: str, error_message: str) -> None:
         pass  # Default to no-op if not overridden
